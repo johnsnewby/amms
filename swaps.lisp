64738f5d8a5fbec3da31201309864e47bcdfcf90
John Newby <john@newby.org>
2022-04-21 08:11:03 +0200

    Many things working!

--- swaps.lisp
+++ swaps.lisp
@@ -1,20 +1,15 @@
-(ql:quickload :alexandria)
 (ql:quickload :postmodern)
+(ql:quickload :str)
 (ql:quickload :yason)
 
-(postmodern:connect-toplevel "tezos" "tezos" "tezos" "localhost" :port 5432)
-(setf result (nth 0
-		  (postmodern:query "select * from peppermint.operations limit 1")))
 
-(defun get_address (command)
-   (gethash "to_address" (gethash "args" (yason:parse command))))
-
-(setf target (gethash "to_address" (gethash "args" (yason:parse
-						    (nth 4 result)))))
+;;;; (postmodern:connect-toplevel "tezos" "tezos" "tezos" "localhost" :port 5432)
+(postmodern:connect-toplevel "amm" "amm" "amm" "localhost" :port 5433)
+(setq postmodern:*escape-sql-names-p* nil)
 
 
-(postmodern:doquery (:select 'command :from 'peppermint.operations) (command)
-  (format t "target: ~A\n" (get_address command)))
+;;;;----------------------------------------------------------------------------
+;;;; Uniswap v1 class
 
 (defclass uniswapv1 ()
   ((x :initarg :x :accessor x)
@@ -59,7 +54,66 @@
 	(new-y (- y swapped-y)))
     (make-instance 'uniswapv1 :x new-x :y new-y :k k :cost (slot-value a 'cost))))
 
+;;;;----------------------------------------------------------------------------
+;;;; Classes for loading swaps from disk
 
+(defclass ondisk ()
+  ((schema-name :initarg :schema-name :accessor schema-name)
+   (table-name :initarg :table-name :accessor table-name)
+   (x-col :initarg :x-col :accessor x-col)
+   (y-col :initarg :y-col :accessor y-col)
+   (cost :initarg :cost :accessor cost)
+   (zeroes :initarg :zeroes :accessor zeroes)
+   (query :initform nil :accessor query)))
+
+(defmethod init ((o ondisk))
+  (if (query o)
+      ()
+      (setf (query o) (postmodern:prepare
+			  (str:concat
+			   "SELECT \"" (x-col o) "\", \"" (y-col o) "\" FROM "
+			   "\"" (schema-name o) "\".\"" (table-name o) "\""
+			   "ORDER BY ID DESC LIMIT 1")))))
+
+(defmethod x-in-y ((o ondisk))
+  (/ y x))
+
+(defmethod y-in-x ((o ondisk))
+  (/ x y))
+
+(defmethod ld ((o ondisk)) ; load from the disk, and return an initialized swap object
+  (let* ((result (nth 0 (funcall (query o))))
+	 (x-col (nth 0 result))
+	 (y-col (nth 1 result)))
+    (setf (slot-value o 'x-col) x-col)
+    (setf (slot-value o 'y-col) y-col))
+  o)
+
+;;;;----------------------------------------------------------------------------
+
+(setf vortex-kUSD-XTX
+      (make-instance 'ondisk :schema-name "Vortex kUSD-XTZ DEX"
+			     :table-name "storage"
+			     :x-col "tokenPool" :y-col "xtzPool"
+			     :cost 0.0025 :zeroes 10))
+(init vortex-kUSD-XTX)
+
+(setf vortex-USDtz-XTZ
+      (make-instance 'ondisk :schema-name "Vortex USDtz-XTZ DEX"
+			     :table-name "storage"
+			     :x-col "tokenPool" :y-col "xtzPool"
+			     :cost 0.0025 :zeroes 10))
+(init vortex-USDtz-XTZ)
+(setq _vortex-USDtz-XTZ (ld vortex-USDtz-XTZ))
+(x-in-y _vortex-USDtz-XTZ)
+
+
+(setq x (ld vortex-kUSD-XTX))
+
+;(postmodern:with-schema ("\"PLENTY\"" :if-not-exist :error)
+  (postmodern:query "SELECT * from \"Vortex kUSD-XTZ DEX\".storage LIMIT 1")
+(query vortex-kUSD-XTX)
+(concatenate "foo" "bar")
 
 (setf a-uniswapv1 (init-uniswapv1 17987987987987987987 980989080980980809 0.05))
 (p a-uniswapv1)
