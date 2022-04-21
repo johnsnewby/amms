db03c6921257bac1f3e0df8ee1c7861580c10afc
John Newby <john@newby.org>
2022-04-21 14:28:01 +0200

    Many changes

--- swaps.lisp
+++ swaps.lisp
@@ -13,27 +13,29 @@
 
 (defclass uniswapv1 ()
   ((x :initarg :x :accessor x)
-   (y :initarg :y :accessor x)
-   (k :initarg :k :accessor k)
+   (y :initarg :y :accessor y)
+   (k :initform nil :accessor k)
+   (zeroes :initarg :zeroes :accessor zeroes)
    (cost :initarg :cost :accessor cost)))
 
-(defun init-uniswapv1 (x y cost)
-  (make-instance 'uniswapv1 :x x :y y :k (* x y) :cost cost))
-
-(defun init-uniswapv1 (x y cost)
-  (let ((a (make-instance 'uniswapv1)))
-    (setf (slot-value a 'x) x)
-    (setf (slot-value a 'y) y)
-    (setf (slot-value a 'k) (* x y))
-    (setf (slot-value a 'cost) cost)
-    a))
+(defmethod init ((u uniswapv1))
+  (setf (k u) (* (x u) (y u)))
+  u)
 
 (defmethod p ((a uniswapv1))
-  (format t "x: ~S y: ~S k: ~S cost: ~S\n"
-	  (slot-value a 'x)
-	  (slot-value a 'y)
-	  (slot-value a 'k)
-	  (slot-value a 'cost)))
+  (format t "x: ~S y: ~S k: ~S x-in-y ~$ y-in-x ~$ cost: ~S\n"
+	  (x a)
+	  (y a)
+	  (k a)
+	  (x-in-y a)
+	  (y-in-x a)
+	  (cost a)))
+
+(defmethod x-in-y ((u uniswapv1))
+  (/ (/ (slot-value u 'x) (slot-value u 'y)) (expt 10 (zeroes u))))
+
+(defmethod y-in-x ((u uniswapv1))
+  (/ (/ (slot-value u 'y) (slot-value u 'x)) (expt 10 (zeroes u))))
 
 (defun swap (x y k swapped-x)
   (- y (/ k (+ x swapped-x))))
@@ -44,7 +46,8 @@
 	(k (slot-value a 'k))
 	(new-x (- x swapped-x))
 	(new-y (swap x y k swapped-x)))
-    (make-instance 'uniswapv1 :x new-x :y new-y :k k :cost (slot-value a 'cost))))
+    (make-instance 'uniswapv1 :x new-x :y new-y :k k :cost (slot-value a 'cost)
+		   :zeroes (zeroes a))))
 
 (defmethod swap-y (swapped-y (a uniswapv1))
   (let* ((x (slot-value a 'x))
@@ -52,7 +55,8 @@
 	(k (slot-value a 'k))
 	(new-x (swap y x k swapped-y))
 	(new-y (- y swapped-y)))
-    (make-instance 'uniswapv1 :x new-x :y new-y :k k :cost (slot-value a 'cost))))
+    (make-instance 'uniswapv1 :x new-x :y new-y :k k :cost (slot-value a 'cost)
+		   :zeroes (zeroes a))))
 
 ;;;;----------------------------------------------------------------------------
 ;;;; Classes for loading swaps from disk
@@ -64,30 +68,24 @@
    (y-col :initarg :y-col :accessor y-col)
    (cost :initarg :cost :accessor cost)
    (zeroes :initarg :zeroes :accessor zeroes)
-   (query :initform nil :accessor query)))
+   (query :initform nil :accessor query)
+   (swap-class :initarg :swap-class :accessor swap-class)))
 
-(defmethod init ((o ondisk))
-  (if (query o)
-      ()
-      (setf (query o) (postmodern:prepare
-			  (str:concat
-			   "SELECT \"" (x-col o) "\", \"" (y-col o) "\" FROM "
-			   "\"" (schema-name o) "\".\"" (table-name o) "\""
-			   "ORDER BY ID DESC LIMIT 1")))))
-
-(defmethod x-in-y ((o ondisk))
-  (/ y x))
-
-(defmethod y-in-x ((o ondisk))
-  (/ x y))
-
-(defmethod ld ((o ondisk)) ; load from the disk, and return an initialized swap object
-  (let* ((result (nth 0 (funcall (query o))))
-	 (x-col (nth 0 result))
-	 (y-col (nth 1 result)))
-    (setf (slot-value o 'x-col) x-col)
-    (setf (slot-value o 'y-col) y-col))
+(defmethod init-ondisk ((o ondisk))
+  (setf (query o) (postmodern:prepare
+		      (str:concat
+		       "SELECT \"" (x-col o) "\", \"" (y-col o) "\" FROM "
+		       "\"" (schema-name o) "\".\"" (table-name o) "\""
+		       "ORDER BY ID DESC LIMIT 1")))
   o)
+
+;;;; load the latest from the disk, and return an initialized swap object
+(defmethod get-latest-swap ((o ondisk))
+  (let* ((result (nth 0 (funcall (query o))))
+	 (x-val (nth 0 result))
+	 (y-val (nth 1 result)))
+    (init (make-instance (swap-class o) :x x-val :y y-val :cost (cost o)
+					:zeroes (zeroes o)))))
 
 ;;;;----------------------------------------------------------------------------
 
@@ -95,26 +93,13 @@
       (make-instance 'ondisk :schema-name "Vortex kUSD-XTZ DEX"
 			     :table-name "storage"
 			     :x-col "tokenPool" :y-col "xtzPool"
-			     :cost 0.0025 :zeroes 10))
-(init vortex-kUSD-XTX)
+			     :cost 0.0025 :zeroes 10
+			     :swap-class 'uniswapv1))
 
-(setf vortex-USDtz-XTZ
-      (make-instance 'ondisk :schema-name "Vortex USDtz-XTZ DEX"
-			     :table-name "storage"
-			     :x-col "tokenPool" :y-col "xtzPool"
-			     :cost 0.0025 :zeroes 10))
-(init vortex-USDtz-XTZ)
-(setq _vortex-USDtz-XTZ (ld vortex-USDtz-XTZ))
-(x-in-y _vortex-USDtz-XTZ)
+(setf swap
+      (postmodern:with-connection '("amm" "amm" "amm" "127.0.0.1" :port 5433)
+	(let* ((od (init-ondisk vortex-kUSD-XTX))
+	       (swap (get-latest-swap od)))
+	  swap)))
 
-
-(setq x (ld vortex-kUSD-XTX))
-
-;(postmodern:with-schema ("\"PLENTY\"" :if-not-exist :error)
-  (postmodern:query "SELECT * from \"Vortex kUSD-XTZ DEX\".storage LIMIT 1")
-(query vortex-kUSD-XTX)
-(concatenate "foo" "bar")
-
-(setf a-uniswapv1 (init-uniswapv1 17987987987987987987 980989080980980809 0.05))
-(p a-uniswapv1)
-(p (swap-x 5000000 a-uniswapv1))
+(p swap)
